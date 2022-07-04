79103ba9c3704e9117935222ba40868612340ea6
Fabien <fabienswift@gmail.com>
2022-07-04 17:29:46 -0400

    make function based code

--- src/classBased/components/TodoContainer.js
+++ src/classBased/components/TodoContainer.js
@@ -6,23 +6,7 @@ import InputTodo from "./InputTodo";
 
 class TodoContainer extends React.Component {
   state = {
-    todos: [
-      {
-        id: uuidv4(),
-        title: "Setup development environment",
-        completed: true,
-      },
-      {
-        id: uuidv4(),
-        title: "Develop website and add content",
-        completed: false,
-      },
-      {
-        id: uuidv4(),
-        title: "Deploy to live server",
-        completed: false,
-      },
-    ],
+    todos: [],
   };
   handleChange = (id) => {
     this.setState((prevState) => ({
@@ -57,16 +41,48 @@ class TodoContainer extends React.Component {
       todos: [...this.state.todos, newTodo],
     });
   };
+
+  setUpdate = (updatedTitle, id) => {
+    this.setState({
+      todos: this.state.todos.map(todo => {
+        if (todo.id === id) {
+          todo.title = updatedTitle
+        }
+        return todo
+      }),
+    })
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if(prevState.todos !== this.state.todos) {
+      const temp = JSON.stringify(this.state.todos)
+      localStorage.setItem("todos", temp)
+    }
+  }
+
+  componentDidMount() {
+    const temp = localStorage.getItem("todos")
+    const loadedTodos = JSON.parse(temp)
+    if (loadedTodos) {
+      this.setState({
+        todos: loadedTodos
+      })
+    }
+  }
+
   render() {
     return (
-      <div>
+      <div className="container">
+         <div className="inner">
         <Header />
         <InputTodo addTodoProps={this.addTodoItem} />
         <TodoList
           todos={this.state.todos}
           handleChangeProps={this.handleChange}
           deleteTodoProps={this.delTodo}
+          setUpdate={this.setUpdate}
         />
+        </div>
       </div>
     );
   }
