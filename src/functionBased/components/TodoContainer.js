bb66c87c9c721b45d45d7b8299a9f916e34d0d98
Fabien <fabienswift@gmail.com>
2022-07-04 20:07:09 -0400

    Add navbar, with home and about page

--- src/functionBased/components/TodoContainer.js
+++ src/functionBased/components/TodoContainer.js
@@ -1,8 +1,12 @@
 import React from "react";
+import { Route, Switch } from "react-router-dom";
 import { v4 as uuidv4 } from "uuid";
 import TodoList from "./TodosList";
 import Header from "./Header";
 import InputTodo from "./InputTodo";
+import About from "../pages/About"
+import NotMatch from "../pages/NotMatch"
+import Navbar from "./Navbar"
 
 class TodoContainer extends React.Component {
   state = {
@@ -44,46 +48,46 @@ class TodoContainer extends React.Component {
 
   setUpdate = (updatedTitle, id) => {
     this.setState({
-      todos: this.state.todos.map(todo => {
+      todos: this.state.todos.map((todo) => {
         if (todo.id === id) {
-          todo.title = updatedTitle
+          todo.title = updatedTitle;
         }
-        return todo
+        return todo;
       }),
-    })
-  }
+    });
+  };
 
   componentDidUpdate(prevProps, prevState) {
-    if(prevState.todos !== this.state.todos) {
-      const temp = JSON.stringify(this.state.todos)
-      localStorage.setItem("todos", temp)
+    if (prevState.todos !== this.state.todos) {
+      const temp = JSON.stringify(this.state.todos);
+      localStorage.setItem("todos", temp);
     }
   }
 
   componentDidMount() {
-    const temp = localStorage.getItem("todos")
-    const loadedTodos = JSON.parse(temp)
+    const temp = localStorage.getItem("todos");
+    const loadedTodos = JSON.parse(temp);
     if (loadedTodos) {
       this.setState({
-        todos: loadedTodos
-      })
+        todos: loadedTodos,
+      });
     }
   }
 
   render() {
     return (
-      <div className="container">
-         <div className="inner">
-        <Header />
-        <InputTodo addTodoProps={this.addTodoItem} />
-        <TodoList
-          todos={this.state.todos}
-          handleChangeProps={this.handleChange}
-          deleteTodoProps={this.delTodo}
-          setUpdate={this.setUpdate}
-        />
-        </div>
-      </div>
+          <div className="container">
+            <div className="inner">
+              <Header />
+              <InputTodo addTodoProps={this.addTodoItem} />
+              <TodoList
+                todos={this.state.todos}
+                handleChangeProps={this.handleChange}
+                deleteTodoProps={this.delTodo}
+                setUpdate={this.setUpdate}
+              />
+            </div>
+          </div>
     );
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,18 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import TodoContainer from "./functionBased/components/TodoContainer"
-import "./functionBased/App.css"
+import React from "react";
+import { BrowserRouter, Routes, Route} from "react-router-dom";
+import ReactDOM from "react-dom/client";
+import TodoContainer from "./functionBased/components/TodoContainer";
+import "./functionBased/App.css";
+import NotMatch from "./functionBased/pages/NotMatch";
+import About from "./functionBased/pages/About";
+import Navbar from "./functionBased/components/Navbar";
+import SinglePage from "./functionBased/pages/SinglePage";
 
-
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <TodoContainer />
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route exact path="/" element={<TodoContainer />} />
+        <Route exact path="/about/singlepage" element={<SinglePage />} />
+        <Route exact path="/about" element={<About />} />
+        <Route exact path="*" element={<NotMatch />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-
